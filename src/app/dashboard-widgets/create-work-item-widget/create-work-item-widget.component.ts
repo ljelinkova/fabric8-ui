acff041b9107155285a7dc7531bd10f30b25b866
ljelinkova <ljelinko@redhat.com>
2018-10-04 14:39:03 +0200

    Fix workitems

--- src/app/dashboard-widgets/create-work-item-widget/create-work-item-widget.component.ts
+++ src/app/dashboard-widgets/create-work-item-widget/create-work-item-widget.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { FilterService, WorkItem, WorkItemService } from 'fabric8-planner';
-import { Contexts } from 'ngx-fabric8-wit';
+import { Contexts, Space } from 'ngx-fabric8-wit';
 import { UserService } from 'ngx-login-client';
 import { ConnectableObservable, Observable } from 'rxjs';
 import { map, publishReplay, switchMap, tap } from 'rxjs/operators';
+import { SpacesService } from '../../shared/spaces.service';
 import { filterOutClosedItems, WorkItemsData } from '../../shared/workitem-utils';
 
 @Component({
@@ -15,7 +16,7 @@ export class CreateWorkItemWidgetComponent implements OnInit {
 
   @Input() userOwnsSpace: boolean;
 
-  private _myWorkItems: ConnectableObservable<WorkItem[]>;
+  private _myWorkItems: Observable<WorkItem[]>;
   myWorkItemsCount: Observable<number>;
   contextPath: Observable<string>;
 
@@ -23,32 +24,57 @@ export class CreateWorkItemWidgetComponent implements OnInit {
     private filterService: FilterService,
     private workItemService: WorkItemService,
     private userService: UserService,
+    private spacesService: SpacesService,
     private contexts: Contexts
   ) {}
 
   ngOnInit() {
-    this.contextPath = this.contexts.current.pipe(map(context => context.path));
-    this._myWorkItems = this.userService
-      .getUser().pipe(
-      tap(() => this.workItemService.buildUserIdMap()),
-      switchMap(() => this.userService.loggedInUser),
-      map(user => this.filterService.queryBuilder('assignee', this.filterService.equal_notation, user.id)),
-      switchMap(filters => this.workItemService
-        .getWorkItems(100000, {expression: filters})),
-      map((val: WorkItemsData) => val.workItems),
-      map(workItems => filterOutClosedItems(workItems)),
-      // Resolve the work item type
-      tap(workItems => workItems.forEach(workItem => this.workItemService.resolveType(workItem))),
-      tap(workItems => workItems.forEach(workItem => this.workItemService.resolveAreaForWorkItem(workItem))),
-      // MUST DO creator after area due to bug in planner
-      tap(workItems => workItems.forEach(workItem => this.workItemService.resolveCreator(workItem))),
-      publishReplay(1)) as ConnectableObservable<WorkItem[]>;
-    this._myWorkItems.connect();
-    this.myWorkItemsCount = this._myWorkItems.pipe(map(workItems => workItems.length));
+    console.log('Hello 1');
+    this.contextPath = this.contexts.current.map(context => context.path);
+    this.workItemService.buildUserIdMap();
+    let userID = this.userService.currentLoggedInUser.id;
+
+    this._myWorkItems = this.spacesService.current.switchMap(space => {
+        let filters = this.createWorkItemQuery(space.id, userID);
+        return this.getWorkItems(filters);
+    });
+    console.log('Hello 3');
+    this.myWorkItemsCount = this._myWorkItems.map(workItems => workItems.length);
+    console.log('Hello 4');
   }
 
   get myWorkItems(): Observable<WorkItem[]> {
     return this._myWorkItems;
   }
 
+  createWorkItemQuery(spaceID: string, userID: string):any {
+    const assigneeQuery = this.filterService.queryJoiner(
+      {},
+      this.filterService.and_notation,
+      this.filterService.queryBuilder(
+        'assignee', this.filterService.equal_notation, userID
+      )
+    );
+
+    const spaceQuery = this.filterService.queryBuilder(
+      'space', this.filterService.equal_notation, spaceID
+    );
+
+    const filters = this.filterService.queryJoiner(
+      assigneeQuery, this.filterService.and_notation, spaceQuery
+    );
+  }
+
+  getWorkItems(filters: any) {
+    return this.workItemService.getWorkItems(100000,  {expression: filters}).
+    map((val: WorkItemsData) => val.workItems).
+    map(workItems => filterOutClosedItems(workItems)).
+    do(() => console.log('Hello 2')).
+    // Resolve the work item type
+    do(workItems => workItems.forEach(workItem => this.workItemService.resolveType(workItem))).
+    do(workItems => workItems.forEach(workItem => this.workItemService.resolveAreaForWorkItem(workItem))).
+    // MUST DO creator after area due to bug in planner
+    do(workItems => workItems.forEach(workItem => this.workItemService.resolveCreator(workItem)));
+  }))
+  }
 }
